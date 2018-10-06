08657ca36dc34580f09e5b1ed1ed0f77714c7326
wisdomman <wise_cc@outlook.com>
2018-10-06 22:59:31 +0800

    node params port

---
+++ node/nodeparams.go
@@ -0,0 +1,21 @@
+package node
+
+
+const (
+
+	DefaultListenPort = 9585 // Default Listen Port for the P2P
+	DefaultListenPortTest = 19585 //Default Listen Port for the P2P Test
+
+	DefaultHTTPRPCHost = "localhost" // Default host for the HTTP RPC server
+	DefaultHTTPRPCPort = 9586        // Default TCP port for the HTTP RPC server
+
+	DefaultHTTPRPCHostTest = "localhost" //Default host for the HTTP RPC Test server
+	DefaultHTTPRPCPortTest = 19586 //Default TCP port for the HTTP RPC Test server
+
+	DefaultWSHost   = "localhost" // Default host interface for the websocket RPC server
+	DefaultWSPort   = 9588        // Default TCP port for the websocket RPC server
+
+	DefaultWSHostTest   = "localhost" // Default host interface for the websocket RPC Test server
+	DefaultWSPortTest   = 19588        // Default TCP port for the websocket RPC Test server
+
+)
